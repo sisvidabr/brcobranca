04320bcb9bb94a43ab5506e71bc7cc4813190b28
Gabriel Rios <gabrielfalcaorios@gmail.com>
2016-02-25 15:47:09 -0300

    Fixing transliterate for ruby 1.8.7

--- lib/brcobranca/formatacao_string.rb
+++ lib/brcobranca/formatacao_string.rb
@@ -1,9 +1,24 @@
+require 'active_support/multibyte/chars'
 require 'active_support/core_ext/string/filters'
-require 'active_support/inflector/transliterate'
+require 'active_support/inflector'
+
+$KCODE = 'UTF8'
 
 module Brcobranca
   # Métodos auxiliares de formatação de strings
   module FormatacaoString
+    # Copied from Rails3 sourcecode
+    def truncate(length, options = {})
+      text = self.dup
+      options[:omission] ||= "..."
+
+      length_with_room_for_omission = length - options[:omission].mb_chars.length
+      chars = text.mb_chars
+      stop = options[:separator] ?
+        (chars.rindex(options[:separator].mb_chars, length_with_room_for_omission) || length_with_room_for_omission) : length_with_room_for_omission
+
+      (chars.length > length ? chars[0...stop] + options[:omission] : text).to_s
+    end
     # Formata o tamanho da string
     # para o tamanho passado
     # se a string for menor, adiciona espacos a direita
@@ -11,9 +26,9 @@ module Brcobranca
     #
     def format_size(size)
       if self.size > size
-        ActiveSupport::Inflector.transliterate(truncate(size, :omission => ''))
+        ActiveSupport::Inflector.transliterate(truncate(size, :omission => '')).to_s
       else
-        ActiveSupport::Inflector.transliterate(ljust(size, ' '))
+        ActiveSupport::Inflector.transliterate(self).to_s.ljust(size, ' ')
       end
     end
   end
