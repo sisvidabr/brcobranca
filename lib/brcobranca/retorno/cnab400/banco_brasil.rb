ac57f3e301565df2fa1105ac0db33dfb72643e3b
Diego Santos <diego@techmobil.com.br>
2018-04-25 11:57:17 -0300

    banco do brasil adicionado ao Base - Retorno

--- lib/brcobranca/retorno/cnab400/banco_brasil.rb
+++ lib/brcobranca/retorno/cnab400/banco_brasil.rb
@@ -9,7 +9,7 @@ module Brcobranca
 
         # Load lines
         def self.load_lines(file, options = {})
-          default_options = { except: [1] } # por padrao ignora a primeira linha que é header
+          default_options = { :except => [1] } # por padrao ignora a primeira linha que é header
           options = default_options.merge!(options)
           super file, options
         end
